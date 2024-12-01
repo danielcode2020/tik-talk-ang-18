60798b72a638f2d56e85c21ffa6acaf00353a91d
daniel <danielmarandici2001@gmail.com>
2024-12-01 22:59:25 +0200

    to continue, 1:23:04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
-import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
-import {ProfileService} from './data/services/profile.service';
-import {HttpClient} from '@angular/common/http';
-import {JsonPipe} from '@angular/common';
-import {Profile} from "./data/interfaces/profile.interface";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent, JsonPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = []
 
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe(val=>{
-      this.profiles = val;
-    })
-    console.log(this.profiles);
-  }
 }
