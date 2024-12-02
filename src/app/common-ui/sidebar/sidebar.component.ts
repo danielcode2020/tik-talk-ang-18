493dd2bd600418a7dc8873a38d87ac912ecbdf56
daniel <danielmarandici2001@gmail.com>
2024-12-02 22:22:49 +0200

    svg works, remained 02:30

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {SvgIconComponent} from "../svg-icon/svg-icon.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
   templateUrl: './sidebar.component.html',
+  imports: [
+    SvgIconComponent
+  ],
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
